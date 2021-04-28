0a4069fae74a0121e67c5de155ddaca8080fef55
Guilherme Cavalcante <cavalcante.guilherme91@gmail.com>
2021-04-28 19:44:03 -0300

    Updating README.md

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'jq66tu@3yu--!$dor*x(+%*-&)dqd#7#c!f2ph%t)=&_pn0i(l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
